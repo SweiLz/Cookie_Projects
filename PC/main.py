a5699f9a48c8b71b95b245a65b81e75622257100
Liews Wuttipat <sweilz.w@gmail.com>
2016-10-16 16:21:30 +0700

    Add Main PC

---
+++ PC/main.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+
+
+
+
+
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret,frame = cap.read()
+    cv2.imshow('frame',frame)
+    key = cv2.waitKey(1)&0xFF
+    if key == 27:
+        break
+
+cv2.destroyAllWindows()
